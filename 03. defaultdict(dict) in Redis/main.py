f5b09a3aa2724c22695581d3e7a6dc13aeb6f3f9
saikaushik003 <115912054+saikaushik003@users.noreply.github.com>
2026-09-23 22:21:21 +0530

    Add Redis dict / hash example in main.py
    Implement Redis hashes / dicts using Python.

---
+++ 03. defaultdict(dict) in Redis/main.py
@@ -0,0 +1,34 @@
+import redis
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
+
+r.flushdb()
+
+
+r.hset("user", mapping = {"name": "Kaushik", "age": 21, "city": "Hyd"})
+
+
+name = r.hget("user", "name")
+print(name)
+
+
+user_details = r.hgetall("user")
+print(user_details)
+
+
+r.hdel("user", "age")
+print(r.hgetall("user"))
+
+
+check = r.hexists("user", "name")
+
+keys = r.hkeys("user")
+print(keys)
+
+vals = r.hvals("user")
+print(vals)
+
